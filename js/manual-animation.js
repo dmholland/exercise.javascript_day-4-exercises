9f71bbc483905917118b4b29a79fae7d3a4f1c6e
dmholland <kenoshatrader@gmail.com>
2020-07-06 14:42:36 -0400

    My own version of how to fix, easy, but Steve is better

--- js/manual-animation.js
+++ js/manual-animation.js
@@ -4,6 +4,7 @@ for(var i=0; i<buttons.length;i++) {
     currentButton.addEventListener('mouseenter', () => changeToCoral(event.target));
     currentButton.addEventListener('mouseleave', () => backToNormal(event.target));
     currentButton.addEventListener('click', ()=> moveImage(event.target.id))
+    
 }
 
 function changeToCoral(eventTarget) {
@@ -24,23 +25,23 @@ function moveImage(direction) {
 
     if(direction === 'KeyD') {        
         imgObjStyleDirection = imgObjStyle.left;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) + 10 + 'px';
+        imgObjStyle.left = parseInt(imgObjStyleDirection) + 10 + 'px';
     }
 
     if(direction === 'KeyA') {
         imgObjStyleDirection = imgObjStyle.left;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) - 10 + 'px';
+        imgObjStyle.left = parseInt(imgObjStyleDirection) - 10 + 'px';
     }
 
     if(direction === 'KeyW') {
         imgObjStyleDirection = imgObjStyle.top;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) - 10 + 'px';
+        imgObjStyle.top = parseInt(imgObjStyleDirection) - 10 + 'px';
     }
 
     
     if(direction === 'KeyS') {
         imgObjStyleDirection = imgObjStyle.top;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) + 10 + 'px';
+        imgObjStyle.top = parseInt(imgObjStyleDirection) + 10 + 'px';
     }
 }
 
